f52931fad56a765d24ee99768cd420f6234c36d9
Maurício Farias <53566332+mauriciompf@users.noreply.github.com>
2023-08-09 19:56:02 -0400

    Update validation.inc.php

--- validation.inc.php
+++ validation.inc.php
@@ -5,19 +5,33 @@ $name = $email = $gender = $comment = $website = "";
 $nameErr = $emailErr = $genderErr = $websiteErr = "";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
-    if (empty($_POST["name"])) $nameErr = "Name is required";
-    else $name = test_input($_POST["name"]);
+    if (empty($_POST["name"])) {
+        $nameErr = "Name is required";
+    }
+    else {
+        $name = test_input($_POST["name"]);
+        if(!preg_match("/[a-zA-Z][a-zA-Z0-9-_]{3,32}/", $name)) $nameErr = "Only letters and white space allowed";
+    }
 
-    if (empty($_POST["email"])) $emailErr = "Email is required";    
-    $email = test_input($_POST["email"]);
+    if (empty($_POST["email"])) {
+        $emailErr = "Email is required";
+    } else {
+        $email = test_input($_POST["email"]);
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) $emailErr = "Invalid email format";
+    }
 
-    if (empty($_POST["website"])) $websiteErr = "Website url is required";
-    else $website = test_input($_POST["website"]);
+    if (empty($_POST["website"])) {
+        $websiteErr = "Website url is required";   
+    } else {
+        $website = test_input($_POST["website"]);
+        if (!preg_match("/\b(?:(?:https?|ftp):\/\/|www\.)[-a-z0-9+&@#\/%?=~_|!:,.;]*[-a-z0-9+&@#\/%=~_|]/i", $website)) $websiteErr = "Invalid URL";
+    }
+    
+    if (empty($_POST["comment"])) $comment = "";
+    else $comment = test_input($_POST["comment"]);
 
     if (empty($_POST["gender"])) $genderErr = "Gender is Required";
     else $gender = test_input($_POST["gender"]);
-
-    $comment = test_input($_POST["comment"]);
 }
 
 function test_input($data) {
